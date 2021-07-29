e9c72b43840a7c1271627fea89843d73193bd2a8
Adarsh Gupta <adarsh@carwale.com>
2021-07-29 18:55:46 +0530

    initial commit

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -12,9 +12,12 @@ namespace GrpcGreeterClient
             // The port number(5001) must match the port of the gRPC server.
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
-           
+           Console.WriteLine("Enter number1 ");
+           int num1 = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Enter number2 ");
+           int num2 = Convert.ToInt32(Console.ReadLine());
             var reply = await client.SayHelloAsync(
-                              new HelloRequest { Num1=30, Num2=10 });
+                              new HelloRequest { Num1=num1, Num2=num2 });
             Console.WriteLine("Answer "+reply.Result);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
